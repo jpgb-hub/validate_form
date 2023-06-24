1ac001094367ed3e333de2c0ac38b640b242b572
jpgb-hub <jpgbonnet@gmail.com>
2023-06-24 14:03:58 -0400

    guarda

--- src/app.js
+++ src/app.js
@@ -4,110 +4,112 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  let form = document.querySelector("form");
-  let soloLetras = /^[a-zA-Z\s]+$/;
-  let soloNumero = /^[0-9]+$/;
 
-  form.addEventListener("submit", event => {
-    event.preventDefault(); // evitar el comportamiento del formulario (enviar los datos);
+  window.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector(".container");
 
-    let hasError = false; // variable para saber si tenemos errores;
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
 
-    //target info
-    let form = event.target;
-    console.log(form.id);
+      var errorMessages = document.querySelectorAll(".error-message");
+      errorMessages.forEach(function(errorMessage) {
+        errorMessage.textContent = "";
+      });
 
-    let {
-      card,
-      ccv,
-      amount,
-      firstname,
-      lastname,
-      city,
-      postalcode,
-      enviar
-    } = container;
-    console.log(card.value);
-    console.log(ccv.value);
-    console.log(amount.value);
-    console.log(firstname.value);
-    console.log(lastname.value);
-    console.log(city.value);
-    console.log(postalcode.value);
-    console.log(enviar.value);
+      // Get the form fields//
+      var card = document.querySelector('input[name="card number"]');
+      var ccv = document.querySelector('input[name="CVC"]');
+      var amount = document.querySelector('input[name="Amount"]');
+      var firstname = document.querySelector('input[name="First name"]');
+      var lastname = document.querySelector('input[name="Last name"]');
+      var city = document.querySelector('input[name="City"]');
+      var postalcode = document.querySelector('input[name="PostalCode"]');
 
-    if (card.value === "") {
-      card.classList.add("is-invalid");
-      hasError = true; // indicamos si tenemos error;
-    } else if (!soloNumero.test(card.value)) {
-      card.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      card.classList.remove("is-invalid");
-    }
+      // Regular expression patterns
+      var soloNumero = /^\d+$/;
+      var soloLetras = /^[A-Za-z]+$/;
 
-    if (ccv.value === "") {
-      ccv.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloNumero.test(ccv.value)) {
-      ccv.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      ccv.classList.remove("is-invalid");
-    }
+      // Validation
+      var hasError = false; // Flag to track if there are any errors
 
-    if (amount.value === "") {
-      amount.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloNumero.test(amount.value)) {
-      amount.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      amount.classList.remove("is-invalid");
-    }
+      if (card.value === "") {
+        card.classList.add("is-invalid");
+        document.getElementById("CardError").textContent =
+          "Card number must be filled out";
+        hasError = true;
+      } else if (!soloNumero.test(card.value)) {
+        card.classList.add("is-invalid");
+        document.getElementById("CardError").textContent =
+          "Card number must contain only numbers";
+        hasError = true;
+      } else {
+        card.classList.remove("is-invalid");
+      }
 
-    if (firstname.value === "") {
-      firstname.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloLetras.test(firstname.value)) {
-      firstname.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      firstname.classList.remove("is-invalid");
-    }
+      if (ccv.value === "") {
+        ccv.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloNumero.test(ccv.value)) {
+        ccv.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        ccv.classList.remove("is-invalid");
+      }
 
-    if (firstname.value === "") {
-      lastname.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloLetras.test(lastname.value)) {
-      lastname.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      lastname.classList.remove("is-invalid");
-    }
+      if (amount.value === "") {
+        amount.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloNumero.test(amount.value)) {
+        amount.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        amount.classList.remove("is-invalid");
+      }
 
-    if (city.value === "") {
-      city.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloLetras.test(city.value)) {
-      city.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      city.classList.remove("is-invalid");
-    }
+      if (firstname.value === "") {
+        firstname.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloLetras.test(firstname.value)) {
+        firstname.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        firstname.classList.remove("is-invalid");
+      }
 
-    if (postalcode.value === "") {
-      postalcode.classList.add("is-invalid");
-      hasError = true;
-    } else if (!soloNumero.test(postalcode.value)) {
-      postalcode.classList.add("is-invalid");
-      hasError = true;
-    } else {
-      postalcode.classList.remove("is-invalid");
-    }
+      if (lastname.value === "") {
+        lastname.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloLetras.test(lastname.value)) {
+        lastname.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        lastname.classList.remove("is-invalid");
+      }
 
-    if (!hasError) {
-      form.submit();
-    }
+      if (city.value === "") {
+        city.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloLetras.test(city.value)) {
+        city.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        city.classList.remove("is-invalid");
+      }
+
+      if (postalcode.value === "") {
+        postalcode.classList.add("is-invalid");
+        hasError = true;
+      } else if (!soloNumero.test(postalcode.value)) {
+        postalcode.classList.add("is-invalid");
+        hasError = true;
+      } else {
+        postalcode.classList.remove("is-invalid");
+      }
+
+      if (!hasError) {
+        // Form is valid, submit it
+        form.submit();
+      }
+    });
   });
 };
